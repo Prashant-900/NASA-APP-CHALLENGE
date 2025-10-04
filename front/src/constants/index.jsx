12d7f1844d71c65cba0d23b189d1e7227b313ccc
Prashant Suthar <tarunsuthar@gmail.com>
2025-10-04 21:32:31 +0530

    url map

--- front/src/constants/index.jsx
+++ front/src/constants/index.jsx
@@ -16,6 +16,6 @@ export const PAGINATION = {
 };
 
 export const API_ENDPOINTS = {
-  BASE_URL: 'https://nasa-app-challenge-3prv.onrender.com/api'
+  BASE_URL: 'https://nasa-app-challenge-1.onrender.com/api'
 };
 
